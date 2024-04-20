fda5d47136028f5deddbb40ca4b48b679aca45e6
akkumaranand <anand.keshri123.ak@gmail.com>
2024-04-20 16:05:01 +0530

    multer

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,36 +1,13 @@
 import multer from "multer";
 import { v4 as uuid } from "uuid";
-import fs from "fs";
-
 const storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, "uploads/temp"); // Save uploads temporarily in the 'uploads/temp' directory
-  },
-  filename(req, file, callback) {
-    const id = uuid();
-    const extName = file.originalname.split(".").pop();
-    callback(null, `${id}.${extName}`);
-  },
+    destination(req, file, callback) {
+        callback(null, "uploads");
+    },
+    filename(req, file, callback) {
+        const id = uuid();
+        const extName = file.originalname.split(".").pop();
+        callback(null, ${id}.${extName});
+    },
 });
-
-const upload = multer({ storage }).single("photo");
-
-// Middleware to handle file upload
-export const singleUpload = (req, res, next) => {
-  upload(req, res, (err) => {
-    if (err) {
-      return res.status(500).json({ error: "Error uploading file" });
-    }
-    // Process the uploaded file here
-    
-    // Cleanup: Delete the temporary file after processing
-    fs.unlink(req.file.path, (err) => {
-      if (err) {
-        console.error('Error deleting temporary file:', err);
-      }
-    });
-
-    next(); // Move to the next middleware or route handler
-  });
-};
-
+export const singleUpload = multer({ storage }).single("photo");
