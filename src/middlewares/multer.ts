2fad8ed0b6e4574d77f31fab25524350e3f442b5
akkumaranand <anand.keshri123.ak@gmail.com>
2024-04-20 17:51:07 +0530

    final commit

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,6 +1,7 @@
 import multer from "multer";
 import { v4 as uuid } from "uuid";
 import fs from "fs";
+import { Request, Response, NextFunction } from 'express'; // Importing types for req, res, and next
 
 const storage = multer.diskStorage({
   destination(req, file, callback) {
@@ -14,15 +15,40 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage }).single("photo");
+// const upload = multer({ storage }).single("photo");
+
+// // Middleware to handle file upload
+// export const singleUpload = (req, res, next) => {
+//    upload(req, res, (err) => {
+//     if (err) {
+//       return res.status(500).json({ error: "Error uploading file" });
+//     }
+//     // Process the uploaded file here
+    
+//     // Cleanup: Delete the temporary file after processing
+//     fs.unlink(req.file.path, (err) => {
+//       if (err) {
+//         console.error('Error deleting temporary file:', err);
+//       }
+//     });
+
+//     next(); // Move to the next middleware or route handler
+//   });
+// };
+
+
+
+
+
 
 // Middleware to handle file upload
-export const singleUpload = (req, res, next) => {
-  upload(req, res, (err) => {
+export const singleUpload = (req: Request, res: Response, next: NextFunction) => {
+   upload(req, res, (err: any) => {
     if (err) {
       return res.status(500).json({ error: "Error uploading file" });
     }
     // Process the uploaded file here
-
+    
     // Cleanup: Delete the temporary file after processing
     fs.unlink(req.file.path, (err) => {
       if (err) {
@@ -32,4 +58,4 @@ export const singleUpload = (req, res, next) => {
 
     next(); // Move to the next middleware or route handler
   });
-};
+}
