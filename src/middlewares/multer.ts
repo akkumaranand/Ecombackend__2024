1e01be81cb54f4f88fb6698a551b5a16309873d1
akkumaranand <anand.keshri123.ak@gmail.com>
2024-04-20 18:16:24 +0530

    final

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,61 +1,16 @@
 import multer from "multer";
 import { v4 as uuid } from "uuid";
-import fs from "fs";
-import { Request, Response, NextFunction } from 'express'; // Importing types for req, res, and next
 
 const storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, "uploads"); // Save uploads temporarily in the 'uploads/temp' directory
-  },
-  filename(req, file, callback) {
-    const id = uuid();
-    const extName = file.originalname.split(".").pop();
-    callback(null, `${id}.${extName}`);
-  },
+    destination(req, file, callback) {
+        callback(null, "uploads");
+    },
+    filename(req, file, callback) {
+        const id = uuid();
+        const extName = file.originalname.split(".").pop();
+        callback(null, `${id}.${extName}`);
+    },
 });
 
-const upload = multer({ storage }).single("photo");
-// const upload = multer({ storage }).single("photo");
+export const singleUpload = multer({ storage }).single("photo");
 
-// // Middleware to handle file upload
-// export const singleUpload = (req, res, next) => {
-//    upload(req, res, (err) => {
-//     if (err) {
-//       return res.status(500).json({ error: "Error uploading file" });
-//     }
-//     // Process the uploaded file here
-    
-//     // Cleanup: Delete the temporary file after processing
-//     fs.unlink(req.file.path, (err) => {
-//       if (err) {
-//         console.error('Error deleting temporary file:', err);
-//       }
-//     });
-
-//     next(); // Move to the next middleware or route handler
-//   });
-// };
-
-
-
-
-
-
-// Middleware to handle file upload
-export const singleUpload = (req: Request, res: Response, next: NextFunction) => {
-   upload(req, res, (err: any) => {
-    if (err) {
-      return res.status(500).json({ error: "Error uploading file" });
-    }
-    // Process the uploaded file here
-    
-    // Cleanup: Delete the temporary file after processing
-    fs.unlink(req.file.path, (err) => {
-      if (err) {
-        console.error('Error deleting temporary file:', err);
-      }
-    });
-
-    next(); // Move to the next middleware or route handler
-  });
-}
